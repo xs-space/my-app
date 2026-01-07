9314fb75d186c9101db217de08a49ca563cb8183
“xs” <w18320724197@163.com>
2026-01-08 00:35:57 +0800

    框架搭建

--- app/config/env.py
+++ app/config/env.py
@@ -1,34 +1,35 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-ENV_FILE = '.env.dev'
+ENV_FILE = r'E:\workspace\github\my_app\.env.dev'
 
 
 class AppSettings(BaseSettings):
-    name: str ='app'
-    version: str = '0.0.0'
+    name: str
+    version: str
+    debug: bool
 
     model_config = SettingsConfigDict(
         env_file=ENV_FILE,
         env_prefix='APP_',
         env_file_encoding='utf-8',
-        case_sensitive=False,  # 不区分大小写,
+        case_sensitive=False,
         extra='ignore',
         validate_default=True
     )
 
 
 class MysqlSettings(BaseSettings):
-    name: str = '127.0.0.1'
-    host: int = 3306
-    username: str = 'root'
-    password: str = '123456'
-    database: str = 'my-app'
+    host: str
+    port: int
+    username: str
+    password: str
+    database: str
 
     model_config = SettingsConfigDict(
         env_file=ENV_FILE,
         env_prefix='MYSQL_',
         env_file_encoding='utf-8',
-        case_sensitive=False,  # 不区分大小写,
+        case_sensitive=False,
         extra='ignore',
         validate_default=True
     )
@@ -43,10 +44,10 @@ class Settings(BaseSettings):
         env_file_encoding='utf-8',
         case_sensitive=False,  # 不区分大小写,
         extra='ignore',
-        validate_default=True
+        validate_default=True  # 验证默认值
     )
 
 
 settings = Settings()
 
-print(settings.app.name)
+print(settings.model_dump())
