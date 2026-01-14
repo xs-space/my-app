f71edd111379d825e65a8f0469be6ce02345d855
“xs” <w18320724197@163.com>
2026-01-14 21:15:01 +0800

    框架搭建

--- app/config/env.py
+++ app/config/env.py
@@ -2,52 +2,50 @@ from pathlib import Path
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-ENV_FILE = Path(__file__).absolute().parent.parent.parent / '.env.dev'
-
-
-class AppSettings(BaseSettings):
-    name: str
-    version: str
-    env: str
-    debug: bool
-
-    model_config = SettingsConfigDict(
-        env_file=ENV_FILE,
-        env_prefix='APP_',
-        env_file_encoding='utf-8',
-        case_sensitive=False,
-        extra='ignore',
-        validate_default=True
-    )
-
-
-class MysqlSettings(BaseSettings):
-    host: str
-    port: int
-    username: str
-    password: str
-    database: str
-
-    model_config = SettingsConfigDict(
-        env_file=ENV_FILE,
-        env_prefix='MYSQL_',
-        env_file_encoding='utf-8',
-        case_sensitive=False,
-        extra='ignore',
-        validate_default=True
-    )
+ENV_FILE = Path(__file__).absolute().parent.parent.parent / ".env.dev"
 
 
 class Settings(BaseSettings):
-    app: AppSettings = AppSettings()
-    mysql: MysqlSettings = MysqlSettings()
+    debug: bool
+
+    # app
+    app_name: str
+    app_version: str
+    app_description: str
+
+    # database
+    db_type: str
+    db_host: str
+    db_port: int
+    db_user: str
+    db_password: str
+    db_name: str
+
+    # redis
+    redis_host: str
+    redis_port: int
+    redis_user: str
+    redis_password: str
+    redis_db: int
+
+    # jwt
+    jwt_secret_key: str
+    jwt_algorithm: str
+    jwt_access_token_expire_minutes: int
+    jwt_refresh_token_expire_minutes: int
+
+    # big model
+    api_key: str
+    model_api_url: str
+    models_available: str
+    default_model: str
 
     model_config = SettingsConfigDict(
         env_file=ENV_FILE,
-        env_file_encoding='utf-8',
+        env_file_encoding="utf-8",
         case_sensitive=False,  # 不区分大小写,
-        extra='ignore',
-        validate_default=True  # 验证默认值
+        extra="ignore",
+        validate_default=True,  # 验证默认值
     )
 
 
